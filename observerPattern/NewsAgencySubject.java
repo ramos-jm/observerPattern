60860538f59f7b914154792e94ea56ebc97f9ccd
John Michael Calitis Ramos <127398189+ramos-jm@users.noreply.github.com>
2023-12-05 04:35:52 +0800

    Delete observerPattern directory

--- observerPattern/NewsAgencySubject.java
+++
@@ -1,8 +0,0 @@
-package SoftEng1.observerPattern;
-
-public interface NewsAgencySubject {
-    void sub(Subscriber subscriber);
-    void unsub(Subscriber subscriber);
-    void notifySubscribers(String news);
-
-}
--- observerPattern/SubscriberObserver.java
+++
@@ -1,9 +0,0 @@
-package SoftEng1.observerPattern;
-
-import java.util.List;
-
-public interface SubscriberObserver {
-    void update(String news);
-
-    void modifyPreference(List<String> preferences);
-}
